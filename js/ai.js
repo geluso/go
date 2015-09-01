e1c15370bd824e2c9efb15d13a61bd54319436ba
Steve Geluso <stevegeluso@gmail.com>
2015-08-31 22:43:56 -0700

    fixing off by one error in random cell selection

--- js/ai.js
+++ js/ai.js
@@ -2,8 +2,8 @@ var RANDOM_MOVE_RATE = 100;
 var DO_RANDOM = false;
 
 function randomMove() {
-  var x = Math.round(Math.random() * GRID);
-  var y = Math.round(Math.random() * GRID);
+  var x = Math.round(Math.random() * (GRID - 1));
+  var y = Math.round(Math.random() * (GRID - 1));
 
   var cell = CELLS[x][y];
   if (cell.stone === EMPTY) {
